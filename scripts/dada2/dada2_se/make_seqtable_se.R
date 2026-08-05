d79ea917d17cb1ba875577bdedb111e66c6c0e9f
head-ease08 <mark.mirny@yandex.ru>
2026-08-05 14:56:42 +0300

    All pe, long read

--- scripts/dada2/dada2_se/make_seqtable_se.R
+++ scripts/dada2/dada2_se/make_seqtable_se.R
@@ -6,8 +6,11 @@ library(dada2)
 
 # No merge step for regions where R1+R2 can't physically overlap (amplicon
 # longer than R1+R2 combined) -- ASV sequences here are forward-read-only.
+files <- snakemake@input$dada_fwd
+names(files) <- sub("_R1\\.rds$", "", basename(files))
+
 # drop samples that had 0 reads survive filtering upstream (NULL placeholder)
-dada_fwd <- lapply(snakemake@input$dada_fwd, readRDS)
+dada_fwd <- lapply(files, readRDS)
 dada_fwd <- dada_fwd[!vapply(dada_fwd, is.null, logical(1))]
 
 saveRDS(
--- scripts/dada2/make_seqtable.R
+++ scripts/dada2/make_seqtable.R
@@ -4,8 +4,11 @@ sink(log, type = "message")
 
 library(dada2)
 
+files <- snakemake@input$merged_reads
+names(files) <- sub("\\.rds$", "", basename(files))
+
 # drop samples that had 0 reads survive filtering upstream (NULL placeholder)
-merged <- lapply(snakemake@input$merged_reads, readRDS)
+merged <- lapply(files, readRDS)
 merged <- merged[!vapply(merged, is.null, logical(1))]
 
 saveRDS(
