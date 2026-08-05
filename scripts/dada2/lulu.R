06f5b86486c7f61f68d3322a5e221cc8ad685a95
head-ease08 <mark.mirny@yandex.ru>
2026-08-05 15:22:41 +0300

    All pe, long read

--- scripts/dada2/lulu.R
+++ scripts/dada2/lulu.R
@@ -7,7 +7,17 @@ if (!requireNamespace("lulu", quietly = TRUE)) {
 }
 library(lulu)
 
-otu_tab   <- read.table(snakemake@input$otu_table,  header = TRUE, row.names = 1, sep = "\t", check.names = FALSE, comment.char = "")
+otu_tab <- read.table(snakemake@input$otu_table,  header = TRUE, row.names = 1, sep = "\t", check.names = FALSE, comment.char = "")
+
+# 0 ASVs survived upstream -- otu_table has 0 rows and match_list.txt is a
+# 0-byte file in that case, which read.table can't parse ("no lines
+# available in input"), and lulu() has nothing to curate anyway.
+if (nrow(otu_tab) == 0) {
+    saveRDS(NULL, snakemake@output$lulu_result)
+    write.table(otu_tab, snakemake@output$curated_table, sep = "\t", quote = FALSE, col.names = NA)
+    quit(save = "no", status = 0)
+}
+
 match_tab <- read.table(snakemake@input$match_list, header = FALSE, sep = "\t")
 
 curated <- lulu(otu_tab, match_tab)
